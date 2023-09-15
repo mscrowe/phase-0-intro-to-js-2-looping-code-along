c70c15a62c5e1ec1b138a1618c91de98bae194b9
Marcello <marcello.crowe@yahoo.com>
2023-09-14 21:12:13 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+
+function writeCards(array, event) {
+    const messages = [];
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+
+
+function countDown() {
+let countDown = 10;
+while (countDown > -1) {
+  console.log(countDown--);
+}
+}
